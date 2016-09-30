4b31139b9d5fcdfde64f4edcfbbf9719023cd2ca
RogerLandOTS <rogerlandots@gmail.com>
2016-09-30 08:37:20 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	$mysql = array(
 		'user' => 'root',
 		'password' => 'RDWPYXjK',
-		'host' => 'localhost',
+		'host' => '192.169.148.131:3306',
 		'database' => 'oexdb'
 	);
 
@@ -12,7 +12,7 @@
 	$rpc = array(
 		'login' => 'dashrpc',
 		'password' => '2Z9MAhm2sk3A8mBJ8AnSuQpVkgjxE3qgWWsHefbnkpy3',
-		'ip' => '127.0.0.1',
+		'ip' => '192.169.148.131',
 		'port' => '9998'
 	);
 	
@@ -28,12 +28,12 @@
 		'income' => 0.8,																			// How much money to send - default: 0.1 - 110%
 		'fee' => 5.00,																				// Fee taken from pay in amount
 		'payout-check' => 180,																// Time between payouts
-		'ownaddress' => '1MSkXPRK293dDMD5ds6KqVtyDadDkRyanX', // Your address
+		'ownaddress' => 'XgKstZyB6tNVgszrc21YGCF5onc7asgo2T', // Your address
 		'sendback' => false,																	// What to do with txs that are over maximum or under minimum | true - send back, false - send to your address
 		'ponziacc' => 'oexdb',																	// Name of daemon account
-		'address' => '1ponzisApJfHtgrwP7CrpfEgseBmPcRD4',			// Ponzi address
-		'privkey' => '',																			// Needed in setup, private key of your address
-		'blockchain-addr' => 'https://blockchain.info/en/address/',
-		'blockchain-tx' => 'https://blockchain.info/en/tx/'
+		'address' => 'Xoz5QJ3nTB5PkonzV6xmmcSVEK2oDydRb9',			// Ponzi address
+		'privkey' => 'XC5YP2ZwxCXrdZFQ7ok6PYaXMed8tQQfY5X2PQ4oCbNZMxic39Ed',																			// Needed in setup, private key of your address
+		'blockchain-addr' => 'https://explorer.dashninja.pl/address/',
+		'blockchain-tx' => 'https://explorer.dashninja.pl/tx/'
 	);
 ?>
